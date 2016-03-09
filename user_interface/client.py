a0e2b439640799cd781479463bffbebceb247683
ChanelAquino <chanelaquino@student.hartnell.edu>
2016-03-09 14:45:01 -0800

    Changed cChanged color scheme and font

--- user_interface/client.py
+++ user_interface/client.py
@@ -4,9 +4,9 @@ from chat_functions import *
 #---------------------------------------------------#
 #---------INITIALIZE CONNECTION VARIABLES-----------#
 #---------------------------------------------------#
-WindowTitle = 'JChat v0.1 - Client'
+WindowTitle = 'Pychat Client'
 HOST = gethostname()
-PORT = 1221
+PORT = 1225
 s = socket(AF_INET, SOCK_STREAM)
 
 #---------------------------------------------------#
@@ -47,7 +47,7 @@ base.geometry("400x500")
 base.resizable(width=FALSE, height=FALSE)
 
 #Create a Chat window
-ChatLog = Text(base, bd=0, bg="white", height="8", width="50", font="Arial",)
+ChatLog = Text(base, bd=0, bg="white", height="8", width="50", font="Courier",)
 ChatLog.insert(END, "Connecting to your partner..\n")
 ChatLog.config(state=DISABLED)
 
@@ -56,20 +56,20 @@ scrollbar = Scrollbar(base, command=ChatLog.yview, cursor="heart")
 ChatLog['yscrollcommand'] = scrollbar.set
 
 #Create the Button to send message
-SendButton = Button(base, font=30, text="Send", width="12", height=5,
-                    bd=0, bg="#FFBF00", activebackground="#FACC2E",
+SendButton = Button(base, font="Courier", text="SEND", width="50", height=5,
+                    bd=0, bg="#81A035", activebackground="#81A035", justify="center",
                     command=ClickAction)
 
 #Create the box to enter message
-EntryBox = Text(base, bd=0, bg="white",width="29", height="5", font="Arial")
+EntryBox = Text(base, bd=0, bg="white",width="29", height="5", font="Courier")
 EntryBox.bind("<Return>", DisableEntry)
 EntryBox.bind("<KeyRelease-Return>", PressAction)
 
 #Place all components on the screen
 scrollbar.place(x=376,y=6, height=386)
 ChatLog.place(x=6,y=6, height=386, width=370)
-EntryBox.place(x=128, y=401, height=90, width=265)
-SendButton.place(x=6, y=401, height=90)
+SendButton.place(x=128, y=401, height=90)
+EntryBox.place(x=6, y=401, height=90, width=265)
 
 
 #---------------------------------------------------#
@@ -79,7 +79,7 @@ SendButton.place(x=6, y=401, height=90)
 def ReceiveData():
     try:
         s.connect((HOST, PORT))
-        LoadConnectionInfo(ChatLog, '[ Succesfully connected ]\n---------------------------------------------------------------')
+        LoadConnectionInfo(ChatLog, '[ Succesfully connected ]\n-------------------------------------')
     except:
         LoadConnectionInfo(ChatLog, '[ Unable to connect ]')
         return
@@ -92,9 +92,9 @@ def ReceiveData():
             break
         if data != '':
             LoadOtherEntry(ChatLog, data)
-            if base.focus_get() == None:
-                FlashMyWindow(WindowTitle)
-                playsound('notif.wav')
+            #if base.focus_get() == None:
+            #    FlashMyWindow(WindowTitle)
+            #    playsound('notif.wav')
 
         else:
             LoadConnectionInfo(ChatLog, '\n [ Your partner has disconnected ] \n')
--- user_interface/host.py
+++ user_interface/host.py
@@ -10,7 +10,7 @@ from chat_functions import *
 WindowTitle = 'Pychat Host'
 s = socket(AF_INET, SOCK_STREAM)
 HOST = gethostname()
-PORT = 1221
+PORT = 1225
 conn = ''
 s.bind((HOST, PORT))
 
@@ -56,7 +56,7 @@ base.geometry("400x500")
 base.resizable(width=FALSE, height=FALSE)
 
 #Create a Chat window
-ChatLog = Text(base, bd=0, bg="white", height="8", width="50", font="Arial",)
+ChatLog = Text(base, bd=0, bg="white", height="8", width="50", font="Courier",)
 ChatLog.insert(END, "Waiting for your partner to connect..\n")
 ChatLog.config(state=DISABLED)
 
@@ -65,20 +65,21 @@ scrollbar = Scrollbar(base, command=ChatLog.yview, cursor="heart")
 ChatLog['yscrollcommand'] = scrollbar.set
 
 #Create the Button to send message
-SendButton = Button(base, font=30, text="Send", width="12", height=5,
-                    bd=0, bg="#FFBF00", activebackground="#FACC2E",
+SendButton = Button(base, font="Courier", text="SEND", width="50", height=5,
+                    bd=0, bg="#81A035", activebackground="#81A035", justify="center",
                     command=ClickAction)
+                    
 
 #Create the box to enter message
-EntryBox = Text(base, bd=0, bg="blue",width="29", height="5", font="Arial")
+EntryBox = Text(base, bd=0, bg="white",width="29", height="5", font="Courier")
 EntryBox.bind("<Return>", DisableEntry)
 EntryBox.bind("<KeyRelease-Return>", PressAction)
 
 #Place all components on the screen
 scrollbar.place(x=376,y=6, height=386)
 ChatLog.place(x=6,y=6, height=386, width=370)
-EntryBox.place(x=128, y=401, height=90, width=265)
-SendButton.place(x=6, y=401, height=90)
+SendButton.place(x=128, y=401, height=90)
+EntryBox.place(x=6, y=401, height=90, width=265)
 
 
 
@@ -89,14 +90,14 @@ def GetConnected():
     s.listen(2)
     global conn
     conn, addr = s.accept()
-    LoadConnectionInfo(ChatLog, 'Connected with: ' + str(addr) + '\n---------------------------------------------------------------')
+    LoadConnectionInfo(ChatLog, 'Connected with: ' + str(addr) + '\n-------------------------------------')
 
     while 1:
         try:
             data = conn.recv(1024)
             LoadOtherEntry(ChatLog, data)
-            if base.focus_get() == None:
-                FlashMyWindow(WindowTitle)
+            #if base.focus_get() == None:
+            #    FlashMyWindow(WindowTitle)
                     #playsound('notif.wav')
         except:
             LoadConnectionInfo(ChatLog, '\n [ Your partner has disconnected ]\n [ Waiting for him to connect..] \n  ')
